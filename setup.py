ee7883fb257d3051c36d0ffd58d2b1eca70606fa
Eymn <eymenyildirim13@icloud.com>
2025-08-01 18:06:16 +0300

    bugfixes

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='Grams_to_KG',
-    version='11.0.0',
-    py_modules=['lib_Grams_to_KG']
+    version='0.1.0',
+    py_modules=['lib_Grams_to_KG'],
+    include_package_data=True
 )
