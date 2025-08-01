ad5276343e526ff2558dec791962a78ccd07500b
Eymn <eymenyildirim13@icloud.com>
2025-08-01 12:22:42 +0300

    files added

---
+++ lib.py.py
@@ -0,0 +1,16 @@
+def grams_to_kg(grams):
+    """Grams to Kilogram."""
+    return grams / 1000
+
+def kg_to_grams(kg):
+    """Kilogram to Grams."""
+    return kg * 1000
+
+def _license():
+    print("licansed under the GNU General Public License v3.0"
+          " (https://www.gnu.org/licenses/gpl-3.0.en.html)")
+    print("\n\nFonctions copied from libemcalc.py")
+
+if __name__ == "__main__":
+_license()
+exit(0)
