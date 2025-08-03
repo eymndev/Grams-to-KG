660e650222bf0f0f08831925b0741a76aad7ef45
Eymn <95621500+eymndev@users.noreply.github.com>
2025-08-03 23:44:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
 setup(
-    name='Grams_to_KG',
+    name='Grams-to-KG',
     version='0.1.0',
-    packages=find_packages(), 
-    include_package_data=True,
+    python_requires<='3.9'
+    author='eymndev',
+    author_email='eymenyildirim13@icloud.com',
+    description='auto convert Grams to KG',
+    url='https://github.com/eymndev/Grams-to-KG',
+    license='GPLv3',
+    install_requires=requirements,
+    py_modules=[
+        'packages=['lib_Grams_to_KG']',
+    ],
 )
