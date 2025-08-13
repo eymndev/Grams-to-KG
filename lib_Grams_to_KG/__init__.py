4b23194b3dc9bc63bd7736727a77549be7e983c2
Eymn <eymenyildirim13@icloud.com>
2025-08-13 18:29:04 +0300

    bugfixes

--- lib_Grams_to_KG/__init__.py
+++ lib_Grams_to_KG/__init__.py
@@ -1,1 +1,2 @@
 from .lib_Grams_to_KG import grams_to_kg
+__all__ = ["grams_to_kg"]
--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
-    name='Grams-to-KG',
-    version='0.1.0',
+    name="grams-to-kg",
+    version="0.1.0",
     python_requires=">=3.8,<4",
-    author='eymndev',
-    author_email='eymenyildirim13@icloud.com',
-    description='auto convert Grams to KG',
-    url='https://github.com/eymndev/Grams-to-KG',
-    license="GPL-3.0",
-    install_requires=requirements,
-    py_modules=["lib_Grams_to_KG"],
+    author="eymndev",
+    author_email="eymenyildirim13@icloud.com",
+    description="auto convert Grams to KG",
+    url="https://github.com/eymndev/Grams-to-KG",
+    license="GPL-3.0-or-later",
+    packages=["lib_Grams_to_KG"],          # ← paket dizini
+    package_dir={"lib_Grams_to_KG": "lib_Grams_to_KG"},
 )
